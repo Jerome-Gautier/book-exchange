62c4d0fea3d8132d8d7cef5305455af83820dd37
Jerome-Gautier <Jerome.gautier92@gmail.com>
2025-07-16 18:50:03 +0200

    Refactored middleware, handle unauthorized api routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,24 @@
-// import { dev } from '$app/environment';
+import { dev } from '$app/environment';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { handle as authenticationHandle } from './auth';
+import { sequence } from '@sveltejs/kit/hooks';
 
-// export function handle({ event, resolve }) {
-// 	if (dev && event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
-// 		return new Response(undefined, { status: 404 });
-// 	}
+const handleDevtools: Handle = async ({ event, resolve }) => {
+    if (dev && event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
+        return new Response(undefined, { status: 404 });
+    }
 
-// 	return resolve(event);
-// }
+    return resolve(event);
+}
 
-export { handle } from "./auth"
+const authorizationHandle: Handle = async ({ event, resolve}) => {
+    if (event.url.pathname.startsWith('/api')) {
+        const session = await event.locals.auth();
+        if (!session) {
+            throw redirect(303, '/users/login');
+        }
+    }
+    return resolve(event);
+}
+
+export const handle: Handle = sequence(handleDevtools, authenticationHandle, authorizationHandle);
